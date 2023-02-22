8e7a9a0a83b0d741627d7c020904e94987759ecc
6im0n <simon.gl3135@orange.fr>
2023-02-22 16:03:29 +0100

    ✅ update bonus

--- bonus.py
+++ bonus.py
@@ -1,6 +1,11 @@
 
 import sys
 from matplotlib import pyplot
+from matplotlib import animation
+
+def update_plot(i, data_list_x, data_list_y, line):
+    line.set_data(data_list_x[:i+1], data_list_y[:i+1])
+    return line,
 
 def main():
     filepath = sys.argv[1]
@@ -11,13 +16,42 @@ def main():
         data_list_x.append(float(data.split(" ")[0]))
         data_list_y.append(float(data.split(" ")[1]))
 
-    print(data_list_x)
-    print(data_list_y)
-    pyplot.plot(data_list_x, data_list_y)
+
+    fig = pyplot.figure()
+    line, = pyplot.plot([], [], lw=2)
     pyplot.xlabel("x")
     pyplot.ylabel("y")
-    pyplot.title("Data buterfly grapher")
+    pyplot.title("Data butterfly grapher")
+
+    anim = animation.FuncAnimation(fig, update_plot, frames=len(data_list_x), fargs=(data_list_x, data_list_y, line), interval=200, blit=True)
+
     pyplot.show()
     file.close()
 
 main();
+
+
+
+
+# import sys
+# from matplotlib import pyplot
+# from matplotlib import animation
+
+
+# def main():
+#     filepath = sys.argv[1]
+#     file = open(filepath, "r")
+#     data_list_x = []
+#     data_list_y = []
+#     for data in file.readlines():
+#         data_list_x.append(float(data.split(" ")[0]))
+#         data_list_y.append(float(data.split(" ")[1]))
+
+#     print(data_list_x)
+#     print(data_list_y)
+#     pyplot.plot(data_list_x, data_list_y)
+#     pyplot.xlabel("x")
+#     pyplot.ylabel("y")
+#     pyplot.title("Data buterfly grapher")
+#     pyplot.show()
+#     file.close()
